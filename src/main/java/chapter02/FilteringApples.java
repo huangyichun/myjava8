07c13687d9d401eb49f25c5349f1fa84d850e1ef
huangyichun <huangyichun@jd.com>
2018-10-22 18:17:38 +0800

    add AppleRedAndHeavyPredicate

--- src/main/java/chapter02/FilteringApples.java
+++ src/main/java/chapter02/FilteringApples.java
@@ -3,6 +3,7 @@ package chapter02;
 import chapter01.Apple;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,6 +11,26 @@ import java.util.List;
  * @date 2018/10/18
  */
 public class FilteringApples {
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple(100, "red"),
+                new Apple(151, "red"),
+                new Apple(88, "green"),
+                new Apple(190, "green"));
+
+        List<Apple> result = filterApples(inventory, new AppleRedAndHeavyPredicate());
+        System.out.println(result);
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
 
     public static List<Apple> filterApplesByColor(List<Apple> inventory, String color) {
         List<Apple> result = new ArrayList<>();
