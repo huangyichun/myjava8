308b02943b5a07bca77c892761da9c86a4889b50
huangyichun <huangyichun@jd.com>
2018-10-29 10:09:05 +0800

    应用环绕执行模式4个步骤

--- src/main/java/chapter03/ExecuteAround.java
+++ src/main/java/chapter03/ExecuteAround.java
@@ -5,6 +5,11 @@ import java.io.*;
 /**
  * @author huangyichun
  * @date 2018/10/29
+ *
+ * 1.行为参数化 （BufferedReaderProcessor接口创建)
+ * 2.使用函数式接口来传递行为 (processFile方法参数设置为BufferedReaderProcessor)
+ * 3.执行一个行为(processFile方法的实现修改为bufferedReaderProcessor.process(br)
+ * 4.传递Lambda （main方法中）
  */
 public class ExecuteAround {
 
@@ -12,13 +17,16 @@ public class ExecuteAround {
     private static String filePath = System.getProperty("user.dir") + "/src/main/java/chapter03/LambdaTest.java";
 
     public static void main(String[] args) throws IOException {
-        String result = processFile();
+        String result = processFile((BufferedReader reader) -> reader.readLine() + reader.readLine());
+        System.out.println(result);
+
+        result = processFile((BufferedReader reader) -> reader.readLine() + reader.readLine() + reader.readLine());
         System.out.println(result);
     }
 
-    public static String processFile() throws IOException {
+    public static String processFile(BufferedReaderProcessor processor) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            return br.readLine();
+            return processor.process(br);
         }
     }
 }
